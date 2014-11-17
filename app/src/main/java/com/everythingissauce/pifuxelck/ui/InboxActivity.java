bf1a5070b426f1c2212659e07c39149b6221a038
Will Coster <willcoster@gmail.com>
2014-11-16 20:10:44 -0800

    Hookup the inbox activity to game models.

--- app/src/main/java/com/everythingissauce/pifuxelck/ui/InboxActivity.java
+++ app/src/main/java/com/everythingissauce/pifuxelck/ui/InboxActivity.java
@@ -1,24 +1,103 @@
 package com.everythingissauce.pifuxelck.ui;
 
-import com.everythingissauce.pifuxelck.Color;
 import com.everythingissauce.pifuxelck.Drawing;
+import com.everythingissauce.pifuxelck.InboxEntry;
 import com.everythingissauce.pifuxelck.R;
+import com.everythingissauce.pifuxelck.Turn;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.view.MotionEvent;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.View;
-import android.widget.GridView;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class InboxActivity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class InboxActivity extends Activity implements
+    AdapterView.OnItemClickListener,
+    SwipeRefreshLayout.OnRefreshListener {
+
+  // Request codes used when launching Activities for results.
+  private static final int REQUEST_DRAWING = 0;
+
+  private InboxAdapter mInboxAdapter;
+  private SwipeRefreshLayout mEntryRefreshLayout;
+  private ListView mEntryListView;
+
+  // TODO(will): Used to store made up turns. This will be removed once a
+  // proper data store and networking has been implemented.
+  private final List<InboxEntry> mEntries = new ArrayList<InboxEntry>();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_inbox);
+
+    addEntry(new Turn(null, "A banana"));
+    addEntry(new Turn(null, "The Death Star!"));
+    addEntry(new Turn(null, "Luke Skywalker eating froyo"));
+
+    mEntryListView = (ListView) findViewById(R.id.entry_list);
+    mEntryListView.setOnItemClickListener(this);
+
+    mEntryRefreshLayout= (SwipeRefreshLayout) findViewById(R.id.refresh_layout);
+    mEntryRefreshLayout.setOnRefreshListener(this);
+    mEntryRefreshLayout.setColorSchemeColors(R.color.accentLight);
+
+    refreshInbox();
+  }
+
+  @Override
+  public void onItemClick(AdapterView<?> parent, View view, int index, long l) {
+    InboxEntry entry = mInboxAdapter.getItem(index);
+    Turn turn = entry.getPreviousTurn();
+
+    if (turn.isLabelTurn()) {
+      Intent drawingIntent = new Intent(DrawingActivity.ACTION_DRAW);
+      drawingIntent.putExtra(DrawingActivity.EXTRAS_LABEL, turn.getLabel());
+      startActivityForResult(drawingIntent, REQUEST_DRAWING);
+    }
+  }
+
+  @Override
+  public void onRefresh() {
+    mEntryRefreshLayout.setRefreshing(false);
+  }
+
+  @Override
+  protected void onActivityResult(int request, int result, Intent data) {
+    switch (request) {
+      case REQUEST_DRAWING:
+        onDrawingActivityResult(result, data);
+        return;
+    }
+  }
+
+  private void refreshInbox() {
+    mInboxAdapter = InboxAdapter.newInboxAdapter(this, mEntries);
+    mEntryListView.setAdapter(mInboxAdapter);
+  }
+
+  private void onDrawingActivityResult(int result, Intent data) {
+    if (result != RESULT_OK) {
+      return;
+    }
+
+    // For now just add it as an inbox entry.
+    Drawing drawing = Drawing.fromBundle(
+        data.getBundleExtra(DrawingActivity.EXTRAS_DRAWING));
+    addEntry(new Turn(null, drawing));
+
+    refreshInbox();
+  }
+
+  // TODO(will): Remove once a real entry data store has been made.
+  private void addEntry(Turn turn) {
+    mEntries.add(new InboxEntry(mEntries.size(), turn));
   }
 }
---
+++ app/src/main/java/com/everythingissauce/pifuxelck/ui/InboxAdapter.java
@@ -0,0 +1,95 @@
+package com.everythingissauce.pifuxelck.ui;
+
+import com.everythingissauce.pifuxelck.InboxEntry;
+import com.everythingissauce.pifuxelck.R;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import com.everythingissauce.pifuxelck.Turn;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Arrays;
+
+/**
+ * A {@link ListAdapter} that will provide views that correspond to the entries
+ * in the player's inbox.
+ */
+public class InboxAdapter extends ArrayAdapter<InboxEntry> {
+
+  public static InboxAdapter newInboxAdapter(
+      Context context, List<InboxEntry> entriesList) {
+    InboxEntry[] entries = new InboxEntry[entriesList.size()];
+    entriesList.toArray(entries);
+
+    // Sort the entries in descending order of their game id.
+    Arrays.sort(entries, new Comparator<InboxEntry>() {
+      @Override
+      public int compare(InboxEntry first, InboxEntry second) {
+        long firstId = first.getGameId();
+        long secondId = second.getGameId();
+        return firstId== secondId ? 0 : firstId < secondId ? 1 : -1;
+      }
+    });
+
+    return new InboxAdapter(context, entries);
+  }
+
+  private InboxAdapter(Context context, InboxEntry[] entries) {
+    super(context, 0, entries);
+  }
+
+  @Override
+  public View getView(int i, View containerView, ViewGroup viewGroup) {
+    Turn turn= getItem(i).getPreviousTurn();
+
+    if (turn.isLabelTurn()) {
+      return getViewForLabel(turn, containerView, viewGroup);
+    } else if (turn.isDrawingTurn()) {
+      return getViewForDrawing(turn, containerView, viewGroup);
+    }
+
+    return null;
+  }
+
+  private View getViewForLabel(Turn turn, View container, ViewGroup group) {
+    TextView labelView;
+
+    // Create a new container if there isn't one that can be recycled, or if the
+    // one that we were given to use is actually a drawing container.
+    if (container == null || (labelView = findLabel(container)) == null) {
+      container = View.inflate(getContext(), R.layout.inbox_label, null);
+      labelView = (TextView) container.findViewById(R.id.label);
+    }
+
+    labelView.setText(turn.getLabel());
+    return container;
+  }
+
+  private TextView findLabel(View container) {
+    return (TextView) container.findViewById(R.id.label);
+  }
+
+  private View getViewForDrawing(Turn turn, View container, ViewGroup group) {
+    DrawingView drawingView;
+
+    // Create a new container if there isn't one that can be recycled, or if the
+    // one that we were given to use is actually a label container.
+    if (container == null || (drawingView = findDrawing(container)) == null) {
+      container = View.inflate(getContext(), R.layout.inbox_drawing, null);
+      drawingView = (DrawingView) container.findViewById(R.id.drawing_view);
+    }
+
+    drawingView.setDrawing(turn.getDrawing());
+    return container;
+  }
+
+  private DrawingView findDrawing(View container) {
+    return (DrawingView) container.findViewById(R.id.drawing_view);
+  }
+}
