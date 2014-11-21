d39cb8351b068c2ee543d6f9edaf73bc1acb0212
Will Coster <willcoster@gmail.com>
2014-11-20 23:11:07 -0800

    Wire up labeling incoming drawings.

--- app/src/main/java/com/everythingissauce/pifuxelck/ui/InboxActivity.java
+++ app/src/main/java/com/everythingissauce/pifuxelck/ui/InboxActivity.java
@@ -4,8 +4,10 @@ import com.everythingissauce.pifuxelck.Drawing;
 import com.everythingissauce.pifuxelck.InboxEntry;
 import com.everythingissauce.pifuxelck.R;
 import com.everythingissauce.pifuxelck.Turn;
+import com.github.pavlospt.CircleView;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -13,16 +15,22 @@ import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class InboxActivity extends Activity implements
     AdapterView.OnItemClickListener,
-    SwipeRefreshLayout.OnRefreshListener {
+    SwipeRefreshLayout.OnRefreshListener,
+    View.OnClickListener, TextView.OnEditorActionListener {
 
   // Request codes used when launching Activities for results.
   private static final int REQUEST_DRAWING = 0;
@@ -30,6 +38,13 @@ public class InboxActivity extends Activity implements
   private InboxAdapter mInboxAdapter;
   private SwipeRefreshLayout mEntryRefreshLayout;
   private ListView mEntryListView;
+
+  private View mOverlayView;
+  private EditText mLabelEditText;
+  private DrawingView mDrawingView;
+
+  private CircleView mNewActionButton;
+  private CircleView mDoneActionButton;
 
   // TODO(will): Used to store made up turns. This will be removed once a
   // proper data store and networking has been implemented.
@@ -52,6 +67,17 @@ public class InboxActivity extends Activity implements
     mEntryRefreshLayout.setOnRefreshListener(this);
     mEntryRefreshLayout.setColorSchemeResources(R.color.accent);
 
+    mOverlayView = findViewById(R.id.overlay_layout);
+    mLabelEditText = (EditText) findViewById(R.id.label_edit_text);
+    mDrawingView = (DrawingView) findViewById(R.id.drawing_view);
+    mLabelEditText.setOnEditorActionListener(this);
+
+    mNewActionButton = (CircleView) findViewById(R.id.new_action_button);
+    mNewActionButton.setOnClickListener(this);
+
+    mDoneActionButton = (CircleView) findViewById(R.id.done_action_button);
+    mDoneActionButton.setOnClickListener(this);
+
     refreshInbox();
   }
 
@@ -64,26 +90,69 @@ public class InboxActivity extends Activity implements
       Intent drawingIntent = new Intent(DrawingActivity.ACTION_DRAW);
       drawingIntent.putExtra(DrawingActivity.EXTRAS_LABEL, turn.getLabel());
       startActivityForResult(drawingIntent, REQUEST_DRAWING);
+    } else if (turn.isDrawingTurn()) {
+      showLabelOverlay(turn.getDrawing());
     }
   }
 
   @Override
-  public void onRefresh() {
-    new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-          @Override
-          public void run() {
-            mEntryRefreshLayout.setRefreshing(false);
-          }
-        }, 5000);
+  public void onClick(View view) {
+    switch (view.getId()) {
+      case R.id.new_action_button:
+        return;
+
+      case R.id.done_action_button:
+        hideLabelOverlay();
+        return;
+    }
   }
 
-    @Override
+  @Override
+  public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
+    if (actionId == EditorInfo.IME_ACTION_DONE) {
+      hideLabelOverlay();
+      return true;
+    }
+    return false;
+  }
+
+  @Override
+  public void onRefresh() {
+    mEntryRefreshLayout.setRefreshing(false);
+  }
+
+  @Override
   protected void onActivityResult(int request, int result, Intent data) {
     switch (request) {
       case REQUEST_DRAWING:
         onDrawingActivityResult(result, data);
         return;
     }
+  }
+
+  private void showLabelOverlay(Drawing drawing) {
+    mOverlayView.setVisibility(View.VISIBLE);
+    mDrawingView.setDrawing(drawing);
+    mDrawingView.invalidate();
+    mLabelEditText.setText("");
+  }
+
+  private void hideLabelOverlay() {
+    // Do not dismiss the overlay unless something has been typed into the box.
+    if (mLabelEditText.getText().length() == 0) {
+      return;
+    }
+
+    mOverlayView.setVisibility(View.INVISIBLE);
+
+    // Make the keyboard go away if it is not already hidden.
+    InputMethodManager imm =
+        (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+    imm.hideSoftInputFromWindow(mLabelEditText.getWindowToken(), 0);
+
+    // TODO(will): For now also add the new label to the turns list.
+    addEntry(new Turn(null, mLabelEditText.getText().toString()));
+    refreshInbox();
   }
 
   private void refreshInbox() {
@@ -96,7 +165,7 @@ public class InboxActivity extends Activity implements
       return;
     }
 
-    // For now just add it as an inbox entry.
+    // TODO(will): For now just add it as an inbox entry.
     Drawing drawing = Drawing.fromBundle(
         data.getBundleExtra(DrawingActivity.EXTRAS_DRAWING));
     addEntry(new Turn(null, drawing));
