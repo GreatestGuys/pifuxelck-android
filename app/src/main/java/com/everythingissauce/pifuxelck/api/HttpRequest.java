abaa8000408796b5cfda221a82405425e1bc9ce9
Will Coster <willcoster@gmail.com>
2014-11-24 23:00:35 -0800

    Fix bug where POST bodies were not sent.

--- app/src/main/java/com/everythingissauce/pifuxelck/api/HttpRequest.java
+++ app/src/main/java/com/everythingissauce/pifuxelck/api/HttpRequest.java
@@ -14,6 +14,7 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.net.HttpURLConnection;
@@ -122,9 +123,10 @@ class HttpRequest {
         if (mBody != null) {
           if (DEBUG) Log.i(TAG, "Setting body: " + mBody);
           connection.setDoOutput(true);
-          Writer writer = new OutputStreamWriter(
-              new BufferedOutputStream(connection.getOutputStream()));
-          writer.write(mBody, 0, mBody.length());
+          OutputStream output =
+              new BufferedOutputStream(connection.getOutputStream());
+          IOUtils.write(mBody, output, "UTF-8");
+          output.close();
         }
 
         int responseCode = connection.getResponseCode();
